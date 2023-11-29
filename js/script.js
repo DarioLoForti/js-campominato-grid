2fa29b87f552b4d59e2db1de6ac5820abcec4888
DarioLoForti <lofortidario@gmail.com>
2023-11-29 13:55:08 +0100

    adding ciclo for per la creazione della griglia

--- js/script.js
+++ js/script.js
@@ -8,3 +8,15 @@ function createCell(num){
     return element;
 }
 
+// recupero l'elemento dal file html che contiene la griglia
+
+let grid = document.getElementById("grid");
+
+// eseguo un ciclo for per la creazione di tutte le celle
+
+for(i=1; i<=100; i++){
+    let square = createCell(i);
+
+    grid.appendChild(square);
+    
+}
